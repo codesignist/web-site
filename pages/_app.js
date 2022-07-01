28c8ee6c588bac9259e2f1e239716093533f20d7
yunus <yunuses@newmind.ai>
2022-07-01 04:56:39 +0300

    Meta tags added

--- pages/_app.js
+++ pages/_app.js
@@ -5,8 +5,19 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <meta charset="utf-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, user-scalable=no"
+        />
         <title>Yunus Eş</title>
         <link rel="shortcut icon" href="/favicon.ico" />
+        <meta property="og:site_name" content="Yunus Eş" key="site_name" />
+        <meta property="og:title" content="Yunus Eş" />
+        <meta property="og:description" content="Kişisel websitesi" />
+        <meta name="twitter:site" content="@codesignist" />
+        <meta name="twitter:title" content="Yunus Eş" />
+        <meta name="twitter:description" content="Kişisel websitesi" />
       </Head>
       <GlobalStyle />
       <Component {...pageProps} />
